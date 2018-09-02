dcddeef71f186729739b3e75b8fa4b543d761bb0
Steven <peragwin@gmail.com>
2018-09-01 23:21:22 -0700

    had added a board main

--- board/main.py
+++ board/main.py
@@ -1,2 +1,28 @@
-from board.proto0 import ProtoBoard
-pb = ProtoBoard()
+#from board.proto0 import ProtoBoard
+#pb = ProtoBoard()
+
+import network
+import machine
+import os
+import time
+
+os.sdconfig(os.SDMODE_1LINE)
+os.mountsd()
+
+sta = network.WLAN()
+sta.active(True)
+sta.connect('dlink-AF94', 'aipwz24505')
+
+connected = False
+for i in range(10):
+    if sta.isconnected():
+        connected = True
+        break
+    time.sleep(1)
+
+if connected:
+    network.ftp.start(user='micro', password='python')
+    network.telnet.start()
+    rtc = machine.RTC()
+    rtc.ntp_sync(server='pool.ntp.org')
+    machine.Pin(21, machine.Pin.OUT).value(1)
